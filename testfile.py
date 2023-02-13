90f2700f439e7663f45688d18a21d8f0f688ae53
Hemanthsravan <99629705+Hemanthsravan@users.noreply.github.com>
2023-02-13 13:34:01 +0530

    Create testfile.py
    # Branches and Road map for Data Science

---
+++ testfile.py
@@ -0,0 +1,22 @@
+# Branches in Data Science
+
+course = print("Please select a course number")
+
+if course == 0:
+  print("Statistics")
+elif course == 1:
+  print("Probability, Distributions and Linear Algebra")
+elif course == 2:
+  print("Python or R")
+elif course == 3:
+  print(" Data Analytics")
+elif course == 4:
+  print("Machine Learning")
+elif course == 5:
+  print(" Neural Networks or Deep Learning")
+elif course == 6:
+  print("Artificial Intelligence")
+elif course == 7:
+  print("Computer Vision")
+else:
+  print(" Data Science Complete Course")
